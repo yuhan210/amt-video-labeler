2933e7de2875ab5d7309701c7e5551e45775ad86
Tiffany Chen <yuhan210@gmail.com>
2015-11-13 01:32:46 -0500

    video pair-wise comparison

--- js/startup.js
+++ js/startup.js
@@ -12,7 +12,7 @@ function StartAMT() {
 		setPageFormat();
 		html5_supported = check_html5_video();
 		if (html5_supported) {
-			loadVideo();
+			loadVideos();
 			renderButtons();
 		}
 
@@ -28,18 +28,16 @@ function StartAMT() {
 function setPageFormat(){
 		
 		// Put image and choice side-by-side
-		$('#anno_region').append("<div id = 'loading'> Loading video, please wait...    </div>" + 
-										 "<div id = 'video'>  </div>" +
+		$('#anno_region').append("<div id = 'loading'> Loading videos, please wait...    </div>" +
+										 "<div id = 'video_container'> </div>" +
 										 "<div id = 'choice'> </div>" +
-										 "<br style='clear: left;' />" +
+	   								 "<div id = 'submit_region'> </div>" + 
 										 "<br> </br> "  
 										);
 			
 		$('#anno_region').width('100%');
-		$('#loading').css({'float':'left'});
-		$('#video').css({'float':'left'});
-		document.getElementById('video').style.display = 'none';
-		$('#choice').css({'float':'left'});
+		$('#video_container').width('100%');
+		document.getElementById('video_container').style.visibility = 'hidden';
 
 };
 
@@ -50,12 +48,10 @@ function check_html5_video(){
 	if(v.canPlayType && v.canPlayType('video/mp4').replace(/no/, '')) {
        supported = true;
 	}
-
 	if (supported == false){
 
 		var html_str = "Your browser does not support HTML5 video.";
 		$('#loading').text(html_str);
-   	document.getElementById('main_media').style.visibility = 'visible';
 
 		return false;
 	}
@@ -63,30 +59,65 @@ function check_html5_video(){
 	return true;
 };
 
-function loadVideo(){
 
-	var video_name = page.video_name;
-	var video_url = 'https://elmo.csail.mit.edu/visual-hint/videos/' + video_name
-	var html_str = '<video width="560" controls>'
-					+  '  <source src="' + video_url + '" type="video/mp4" id = "myVideo">'
-					+  'Your browser does not support HTML5 video.'
-					+ '</video>'
-					;
-	$('#video').append(html_str);	
+function getVideoURL(video_name) {
 
-	var v = document.getElementsByTagName('video')[0];
-	v.onended = function(e) {
-		video_finished = true;
+	return 'https://elmo.csail.mit.edu/visual-hint/videos/' + video_name;
+}
+
+function canPlayVideoA() {
+		console.log('a is loaded');
+		video_loaded_a = true;
+		setLoadingMsgVisibility();
+}
+
+function canPlayVideoB() {
+		console.log('b is loaded');
+		video_loaded_b = true;
+		setLoadingMsgVisibility();
+}
+function endedVideoA() {
+		console.log('a has ended');
+		video_finished_a = true;
 		setSubmitButtonVisibility();
 		$("#mt_submit").text('Submit HIT');
-	}	
-	v.oncanplay = function(e) {
-		document.getElementById('loading').style.display = 'none';
-		document.getElementById('video').style.display = 'block';
-	}
+}
+
+function endedVideoB() {
+		console.log('b has ended');
+		video_finished_b = true;
+		setSubmitButtonVisibility();
+		$("#mt_submit").text('Submit HIT');
+}
+function loadVideos(){
+
+	var video_name_a = page.video_name_a;
+	var video_name_b = page.video_name_b;
+	var video_url_a = getVideoURL(video_name_a);
+	var video_url_b = getVideoURL(video_name_b);
+	var html_str = '<table style = "width:100%">' 
+					+  '<tr> <td align = "center"><font size = 8> <b>Video A</b></td>'
+   			   +  '     <td align = "center"><font size = 8> <b>Video B</b></td></tr>'
+					+  '<tr><td align = "center"> <video width="560" controls oncanplay="canPlayVideoA()" onended = "endedVideoA()">'
+					+  '<source src="' + video_url_a + '" type="video/mp4" id = "videoa">'
+					+  'Your browser does not support HTML5 video.'
+					+  '</video> </td> '
+					+  '<td align = "center"> <video width="560" controls oncanplay="canPlayVideoB()" onended = "endedVideoB()">'
+					+  '<source src="' + video_url_b + '" type="video/mp4" id = "videob">'
+					+  'Your browser does not support HTML5 video.'
+					+  '</video> </td></tr> </table>';
+	
+	$('#video_container').append(html_str);	
+	
 
 }
 
+function setLoadingMsgVisibility(){
+	if (video_loaded_a && video_loaded_b) {
+		document.getElementById('loading').style.display = 'none';
+		document.getElementById('video_container').style.visibility = 'visible';
+	}
+}
 
 function onChange(){
 	var items = document.getElementsByName("score");
@@ -102,27 +133,23 @@ function onChange(){
 
 function renderButtons(){
 
-	var intro_str = '<table>'
+	var intro_str = '<br><table>'
 			+ '<tr><td>'
-			+ '<font size="4"><b> On a scale of 0-5, how relevant is this video to </font> <font size = 6> <u>"dog"</u></font>? </b></font>'         
+			+ '<font size="4"><b> Which video is more relevant to the word: </font> <font size = 6> <u>"dog"</u></font> ? </b></font>'         
 		   + '</tr></td>' 
 			+ '</table>';
 	$('#choice').append(intro_str);
 	var select_html_str = '<form>' +
-								' <input type="radio" name="score" value="0" onchange="onChange()"> 0 - not relevant<br>' +
-								' <input type="radio" name="score" value="1" onchange="onChange()"> 1 <br>' +
-								' <input type="radio" name="score" value="2" onchange="onChange()"> 2 <br>' +
-								' <input type="radio" name="score" value="3" onchange="onChange()"> 3 <br>' +
-								' <input type="radio" name="score" value="4" onchange="onChange()"> 4 <br>' +
-								' <input type="radio" name="score" value="5" onchange="onChange()"> 5 - very relevant<br>' +
+								' <input type="radio" name="score" value="A" onchange="onChange()"> Video A <br>' +
+								' <input type="radio" name="score" value="B" onchange="onChange()"> Video B <br>' +
 								' </form>';
 	$('#choice').append(select_html_str);
 
 	var html_submit_str = ''
-	html_submit_str = '<table>'
+	html_submit_str = '<table style = "width: 100%">'
 					+ '<tr>'
 					+ '<td>'
-					+ '<button disabled="true" type="button" style="height:50px; width:1000px" id="mt_submit" name="Submit" onclick="onSubmit(this);">Submit HIT </button>'
+					+ '<button disabled="true" type="button" style="height:50px; width:100%" id="mt_submit" name="Submit" onclick="onSubmit(this);">Submit HIT </button>'
        			+ '</td>'
 					+ '<td>'
       	 	   + '<form id="amt-form" action="' + submitURL + '">'
@@ -130,16 +157,17 @@ function renderButtons(){
 					+ '<input type="hidden" id="turkSubmitTo" name="turkSubmitTo" value="'+ page.turkSubmitTo +'" />'
 					+ '<input type="hidden" id="hitId" name="hitId" value="'+ page.hitId +'" />'
 					+ '<input type="hidden" id="workerId" name="workerId" value="'+ page.workerId +'" />'
-					+ '<input type="hidden" id="video_name" name="video_name" value="' + page.video_name + '" />'
+					+ '<input type="hidden" id="video_name_a" name="video_name_a" value="' + page.video_name_a + '" />'
+					+ '<input type="hidden" id="video_name_b" name="video_name_b" value="' + page.video_name_b + '" />'
 					+ '<input type="hidden" id="selected_value" name="selected_value" value="" />'
 					+ '<input type="hidden" id="mt_comments" name="mt_comments" value="" />'
 					//+ '<input disabled="true" type="submit" style="height:50px; width:1000px" id="mt_submit" name="Submit" value="Submit HIT" onclick="onSubmit(this);" />'
 						//onmousedown="javascript:document.getElementById(\'mt_comments\').value=document.getElementById(\'mt_comments_textbox\').value;" />'
 					+ '</form>'
 					+ '</td></tr></table>';
-				console.log(html_submit_str);
+				//console.log(html_submit_str);
 
-		$('#anno_region').append(html_submit_str);
+		$('#submit_region').append(html_submit_str);
 
 }
 
@@ -155,13 +183,14 @@ function onSubmit(event){
 
 function setSubmitButtonVisibility(){
 	console.log(selected_value);
-	console.log(video_finished);
+	console.log(video_finished_a);
+	console.log(video_finished_b);
 	
-	if (selected_value > -1 && video_finished) {
+	if (selected_value != null && video_finished_a && video_finished_b) {
 			document.getElementById("mt_submit").disabled = false;
 	}else{
-			if (video_finished == false) {
-				$("#mt_submit").text('Submit HIT (Please finish watching the video)');
+			if (video_finished_a == false || video_finished_b == false) {
+				$("#mt_submit").text('Submit HIT (Please finish watching the videos)');
 			}
 			document.getElementById("mt_submit").disabled = true;
 	}
