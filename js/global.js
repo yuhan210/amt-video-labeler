ea60e894ab4c922a869a68ee3980bc6856636884
Tiffany Chen <yuhan210@gmail.com>
2015-11-24 16:08:29 -0500

    changing variables

--- js/global.js
+++ js/global.js
@@ -2,5 +2,8 @@ var externalSubmitURL = 'https://www.mturk.com/mturk/externalSubmit';
 var externalSubmitURLsandbox = 'https://workersandbox.mturk.com/mturk/externalSubmit';
 var submitURL = externalSubmitURLsandbox; 
 
-var video_finished = false;
-var selected_value = -1;
+var video_finished_a = false;
+var video_finished_b = false;
+var video_loaded_a = false;
+var video_loaded_b = false;
+var selected_value = null;
